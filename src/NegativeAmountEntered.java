c6ad08093569b9fe0f4e96f750ee0c738bd36e65
shannabenammar <shannabenammar@users.noreply.github.com>
2020-05-07 19:56:00 -0400

    Create NegativeAmountEntered.java

---
+++ src/NegativeAmountEntered.java
@@ -0,0 +1,28 @@
+/*
+ * Shanna Benammar
+ * 
+ * Final Exam part one
+ * 
+ * Professor Ziegler 
+ * 
+ */
+package FinalExamClasses;
+
+public class NegativeAmountEntered extends Exception{
+	
+	/*
+	 * Constructor NegativeAmountEntered
+	 * Input:
+	 *  double a: holds the value of the negative amount
+	 * Process:
+	 *  Calls the Exception class constructor, and sends it an 
+	 * 	error message.
+	 * Output:
+	 *  N/A
+	 */
+	public NegativeAmountEntered(double a) {
+		super("Error: " + a + " is a negative amount \nwhich cannot be "
+				+ "deposited into an account");
+	}
+
+}
