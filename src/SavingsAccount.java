80ac98fe3ce5f5c5deb35ccb5231ef48d795ea58
shannabenammar <55299429+shannabenammar@users.noreply.github.com>
2020-05-07 19:59:18 -0400

    Update SavingsAccount.java

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,14 +1,3 @@
-/*
- * Shanna Benammar
- * 
- * Final Exam part one
- * 
- * Professor Ziegler 
- * 
- */
-package FinalExamClasses;
-
-
 public class SavingsAccount extends BankAccount implements Interest{
 	
 	/*
