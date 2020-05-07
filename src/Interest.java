cb9eed9c5e586695334f8d5b07f55aa7d1635ee9
shannabenammar <shannabenammar@users.noreply.github.com>
2020-05-07 19:55:57 -0400

    Create Interest.java

---
+++ src/Interest.java
@@ -0,0 +1,24 @@
+/*
+ * Shanna Benammar
+ * 
+ * Final Exam part one
+ * 
+ * Professor Ziegler 
+ * 
+ */
+package FinalExamClasses;
+
+public interface Interest {
+	
+	/*
+	 * Abstract method addInterest()
+	 * Input:
+	 *  double i: holds the rate of interest amount
+	 * Process:
+	 * 	waiting to be overridden by implementing class
+	 * Output:
+	 * 	N/A
+	 */
+	public void addInterest(double i);
+	
+}
