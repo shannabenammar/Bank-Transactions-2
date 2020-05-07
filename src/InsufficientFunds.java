1c692504e7ba8ec261d63185a719687e9a0c1189
shannabenammar <shannabenammar@users.noreply.github.com>
2020-05-07 19:55:54 -0400

    Create InsufficientFunds.java

---
+++ src/InsufficientFunds.java
@@ -0,0 +1,28 @@
+/*
+ * Shanna Benammar
+ * 
+ * Final Exam part one
+ * 
+ * Professor Ziegler 
+ * 
+ */
+package FinalExamClasses;
+
+public class InsufficientFunds extends Exception{
+
+	/*
+	 * Constructor InsufficientFunds
+	 * Input:
+	 *  double b: holds the value of the number which was to high
+	 * Process:
+	 *  Calls the Exception class constructor, and sends it an 
+	 * 	error message.
+	 * Output:
+	 *  N/A
+	 */
+	public InsufficientFunds(double b) {
+		super("Error: " + b + " is an amount which is"
+				+ "\nmore than the balance of the account");
+	}
+
+}
